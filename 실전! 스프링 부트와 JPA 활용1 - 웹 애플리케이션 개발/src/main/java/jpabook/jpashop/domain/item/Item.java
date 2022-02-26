62aa4ea0b806b75236c03d8cdd749bc77358a558
HoduUlmu <62408879+HoduUlmu@users.noreply.github.com>
2022-02-26 16:58:22 +0900

    상품 도메인 개발

--- 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,18 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // 재고 증가
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    // 재고 감소
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("재고가 충분하지 않습니다");
+        }
+        this.stockQuantity = restStock;
+    }
 }
