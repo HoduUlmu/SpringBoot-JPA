fbc1f195da0e42399a000b785ba461d4c944901f
HoduUlmu <62408879+HoduUlmu@users.noreply.github.com>
2022-03-02 16:32:28 +0900

    API 개발 기본

--- 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/src/main/java/jpabook/jpashop/service/ItemService.java
+++ 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,11 +21,29 @@ public class ItemService {
         itemRepository.save(item);
     }
 
+    // 변경감지가 merge 보다 훨씬 안전, 대신 set 은 쓰지말것
+    @Transactional
+    public void upateItem(Long itemId, Book param) {
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setPrice(param.getPrice());
+        findItem.setName(param.getName());
+        findItem.setStockQuantity(param.getStockQuantity());
+    }
+
     public List<Item> findItems() {
         return itemRepository.findAll();
     }
 
     public Item findOne(Long itemId) {
         return itemRepository.findOne(itemId);
+    }
+
+    public void updateItem(Long itemId, String name, int price, int stockQuantity, String author, String isbn) {
+        Book findItem = (Book) itemRepository.findOne(itemId);
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
+        findItem.setAuthor(author);
+        findItem.setIsbn(isbn);
     }
 }
